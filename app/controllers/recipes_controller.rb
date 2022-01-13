0b74787183d64e0c58764934e12aa92539a0c73a
Wasanta <kwan_u_day@hotmail.com>
2022-01-13 12:26:45 -0500

    done

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,31 @@
+# class RecipesController < ApplicationController
+#     # skip_before_action :authorize, only: :create
+#     def create
+#         recipe = @current_user.recipes.create!(recipe_params)
+#         render json: recipe, status: :created
+#     end
+
+#     private
+#     def recipe_params
+#         params.permit(:title, :instructions, :minutes_to_complete )
+#     end
+# end
+
+class RecipesController < ApplicationController
+  
+    def index
+      render json: Recipe.all
+    end
+  
+    def create
+      recipe = @current_user.recipes.create!(recipe_params)
+      render json: recipe, status: :created
+    end
+  
+    private
+  
+    def recipe_params
+      params.permit(:title, :instructions, :minutes_to_complete)
+    end
+  
+  end
